0679cf2904f45e50961817c7868fa2e59abc6c61
Alex <alex.kilkka@gmail.com>
2020-12-26 11:37:08 -0800

    Replace API with OpenSea on token endpoint

--- api/views.py
+++ api/views.py
@@ -34,20 +34,20 @@ from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from gql import gql, Client
-from gql.transport.requests import RequestsHTTPTransport
+#from gql import gql, Client
+#from gql.transport.requests import RequestsHTTPTransport
+import requests
 
 
 
-
-
+'''
 transport = RequestsHTTPTransport(
     url='https://api.thegraph.com/subgraphs/name/wighawag/eip721-subgraph',
     verify=True,
     retries=3,
 )
 subgraph_client = Client(transport=transport)
-
+'''
 
 def index(request):
     '''
@@ -62,143 +62,173 @@ class TokenView(View):
     Returns a single item for the detail page
     '''
 
-    def get(self, request, contract_token_id):
+    def get(self, request, address, token_id):
+
         '''
         Params: token (required)
         '''
 
-        if not contract_token_id:
+        if not address:
             # Return early with error message
             status_code = 400
             response_body = {
                         "error": {
                             "code": status_code,
-                            "message": "Required parameter missing: contract_token_id"
+                            "message": "Required parameter missing: address"
                         }
                     }
             return JsonResponse(response_body, status=status_code)
 
-        # TBD: Check to see if it's a valid token id format
-        #example: "0x0000000000001b84b1cb32787b0d64758d019317_325953"
-
-        if not bool(re.match(r"0x([0-9a-z]{40})_[0-9]+$", contract_token_id)):
+        if not token_id:
             # Return early with error message
             status_code = 400
             response_body = {
                         "error": {
                             "code": status_code,
-                            "message": "contract_token_id not in expected format"
+                            "message": "Required parameter missing: token_id"
                         }
                     }
             return JsonResponse(response_body, status=status_code)
+
+        # Check to see if it's a valid address format
+        # example: "0x0000000000001b84b1cb32787b0d64758d019317"
+
+        if not bool(re.match(r"0x([0-9a-z]{40})+$", address)):
+            # Return early with error message
+            status_code = 400
+            response_body = {
+                        "error": {
+                            "code": status_code,
+                            "message": "address not in expected format"
+                        }
+                    }
+            return JsonResponse(response_body, status=status_code)
+
 
 
         # Continue with query
-        # Assuming we're not going to get injected based on token format check above
+        response = requests.get("https://api.opensea.io/api/v1/asset/{address}/{token_id}".format(address=address, token_id=token_id))
 
-        query = '''query {{
-                
-                token(id: "{token}") {{
-                    id
-                    contract {{
-                        id
-                    }}
-                    tokenID
-                    owner {{
-                        id
-                    }}
-                    tokenURI
-                }}
-            }}
-        '''.format(token=contract_token_id)
-
-        token_data = subgraph_client.execute(gql(query))
-
-        if not token_data['token']:
-            # Return early with error message
-            status_code = 404
+        if response.status_code!=200:
+            status_code = response.status_code
             response_body = {
-                        "error": {
-                            "code": status_code,
-                            "message": "Token not found"
-                        }
-                    }
+                "error": {
+                    "code": status_code,
+                    "message": "Error from OpenSea API"
+                }
+            }
             return JsonResponse(response_body, status=status_code)
 
 
-        #print(token_data)
-
-        # Check if missing token URI
-        token_uri = token_data['token'].get('tokenURI')
-
-        if not token_uri:
-            # Return early with error message
-            status_code = 402
-            response_body = {
-                        "error": {
-                            "code": status_code,
-                            "message": "Token does not contain URI"
-                        }
-                    }
-            return JsonResponse(response_body, status=status_code)
-
-        # Check if URI not json
-        if len(token_uri)<22 or token_uri[:22] != "data:application/json,":
-            # Return early with error message
-            status_code = 402
-            response_body = {
-                        "error": {
-                            "code": status_code,
-                            "message": "Token URI does not contain json"
-                        }
-                    }
-            return JsonResponse(response_body, status=status_code)
-
-
-        token_uri_json = json.loads(token_uri.replace("data:application/json,",""))
-        #print(token_uri_json)
-
-        name = token_uri_json.get("name")
-        if name:
-            name = urllib.parse.unquote(name)
-        description = token_uri_json.get("description")
-        if description:
-            description = urllib.parse.unquote(description)
-        image = token_uri_json.get("image")
-
-        owner_id = None
-        owner = token_data['token'].get("owner")
-        if owner:
-            owner_id = owner.get("id")
-
-        contract_id = None
-        contract = token_data['token'].get('contract')
-        if contract:
-            contract_id = contract.get("id")
-
-        response_body = {
-                        "data": {
-                            "id": contract_token_id,
-                            "contract_id": contract_id,
-                            "token_id": token_data['token'].get('tokenID'),
-                            "owner_id": owner_id,
-                            "name": name,
-                            "description": description,
-                            "image": image,
-                            #"creator": "Creator",
-                            #"price": "Price",
-                        }
-                    }
-        return JsonResponse(response_body)
+        #For now, just return full opensea API
+        return JsonResponse(response.json())
 
 
 
-    def post(self, request, contract_token_id):
+        # query = '''query {{
+
+        #         token(id: "{address}_{token_id}") {{
+        #             id
+        #             contract {{
+        #                 id
+        #             }}
+        #             tokenID
+        #             owner {{
+        #                 id
+        #             }}
+        #             tokenURI
+        #         }}
+        #     }}
+        # '''.format(address=address, token_id=token_id)
+
+        # token_data = subgraph_client.execute(gql(query))
+
+        # if not token_data['token']:
+        #     # Return early with error message
+        #     status_code = 404
+        #     response_body = {
+        #                 "error": {
+        #                     "code": status_code,
+        #                     "message": "Token not found"
+        #                 }
+        #             }
+        #     return JsonResponse(response_body, status=status_code)
+
+
+        # #print(token_data)
+
+        # # Check if missing token URI
+        # token_uri = token_data['token'].get('tokenURI')
+
+        # if not token_uri:
+        #     # Return early with error message
+        #     status_code = 402
+        #     response_body = {
+        #                 "error": {
+        #                     "code": status_code,
+        #                     "message": "Token does not contain URI"
+        #                 }
+        #             }
+        #     return JsonResponse(response_body, status=status_code)
+
+        # # Check if URI not json
+        # if len(token_uri)<22 or token_uri[:22] != "data:application/json,":
+        #     # Return early with error message
+        #     status_code = 402
+        #     response_body = {
+        #                 "error": {
+        #                     "code": status_code,
+        #                     "message": "Token URI does not contain json"
+        #                 }
+        #             }
+        #     return JsonResponse(response_body, status=status_code)
+
+
+        # token_uri_json = json.loads(token_uri.replace("data:application/json,",""))
+        # #print(token_uri_json)
+
+        # name = token_uri_json.get("name")
+        # if name:
+        #     name = urllib.parse.unquote(name)
+        # description = token_uri_json.get("description")
+        # if description:
+        #     description = urllib.parse.unquote(description)
+        # image = token_uri_json.get("image")
+
+        # owner_id = None
+        # owner = token_data['token'].get("owner")
+        # if owner:
+        #     owner_id = owner.get("id")
+
+        # contract_id = None
+        # contract = token_data['token'].get('contract')
+        # if contract:
+        #     contract_id = contract.get("id")
+
+        # response_body = {
+        #                 "data": {
+        #                     #"id": contract_token_id,
+        #                     "contract_id": contract_id,
+        #                     "token_id": token_data['token'].get('tokenID'),
+        #                     "owner_id": owner_id,
+        #                     "name": name,
+        #                     "description": description,
+        #                     "image": image,
+        #                     #"creator": "Creator",
+        #                     #"price": "Price",
+        #                 }
+        #             }
+        # return JsonResponse(response_body)
+
+
+
+    def post(self, request, address, token_id):
         '''
         Params:
-        1. token (required - in path)
-        2. action (required - in body) - values include "like" & "unlike"
-        3. user_id (required - in body)
+        1. address (required - in path)
+        2. token_id (required - in path)
+        3. action (required - in body) - values include "like" & "unlike"
+        4. user_id (required - in body)
         '''
 
         json_body = json.loads(request.body.decode())
@@ -207,18 +237,41 @@ class TokenView(View):
 
         #action = request.POST.get("action") # this is the syntax for forms
 
-        if not contract_token_id:
+        if not address:
             # Return early with error message
             status_code = 400
             response_body = {
                         "error": {
                             "code": status_code,
-                            "message": "Required parameter missing: token"
+                            "message": "Required parameter missing: address"
                         }
                     }
             return JsonResponse(response_body, status=status_code)
 
-        # TBD: Check to make sure it's a valid token. Return error if not.
+        if not token_id:
+            # Return early with error message
+            status_code = 400
+            response_body = {
+                        "error": {
+                            "code": status_code,
+                            "message": "Required parameter missing: token_id"
+                        }
+                    }
+            return JsonResponse(response_body, status=status_code)
+
+        # Check to see if it's a valid address format
+        # example: "0x0000000000001b84b1cb32787b0d64758d019317"
+
+        if not bool(re.match(r"0x([0-9a-z]{40})+$", address)):
+            # Return early with error message
+            status_code = 400
+            response_body = {
+                        "error": {
+                            "code": status_code,
+                            "message": "address not in expected format"
+                        }
+                    }
+            return JsonResponse(response_body, status=status_code)
 
         if not user_id:
             # Return early with error message
